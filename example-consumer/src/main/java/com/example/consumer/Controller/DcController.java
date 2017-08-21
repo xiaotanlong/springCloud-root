fb337ea2df0413973ad5d64abd993e9abbd4a4c4
tanjianglong <tanjianglong@rayvision.com>
2017-08-21 20:44:07 +0800

    test

---
+++ example-consumer/src/main/java/com/example/consumer/Controller/DcController.java
@@ -0,0 +1,34 @@
+package com.example.consumer.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ProjectName springcloudroot
+ * @PackageName com.example.consumer.Controller
+ * @Author tanjianglong
+ * @CreatedTime 2017/8/11.
+ * @Description :Plase give some message
+ * 修改记录：1...;2....
+ */
+@RestController
+public class DcController {
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer")
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+}
