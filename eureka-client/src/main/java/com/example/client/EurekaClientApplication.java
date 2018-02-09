b6dea12d6deb948d02ab89825ea8259324f36bb0
0217319 <xiaotantjl@163.com>
2018-02-09 17:32:40 +0800

    java spi

--- eureka-client/src/main/java/com/example/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/client/EurekaClientApplication.java
@@ -1,8 +1,11 @@
 package com.example.client;
 
+import com.eureka.common.spi.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,5 +13,13 @@ public class EurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
+
+		//java spi  test
+		ServiceLoader<Log> serviceLoader = ServiceLoader.load(Log.class);
+		Iterator<Log> iterator = serviceLoader.iterator();
+		while (iterator.hasNext()) {
+			Log log = iterator.next();
+			log.execute("iiiiiiiiiiii");
+		}
 	}
 }
---
+++ eureka-common/src/main/java/com/eureka/common/spi/Log.java
@@ -0,0 +1,8 @@
+package com.eureka.common.spi;
+
+/**
+ * java spi 接口类
+ */
+public interface Log {
+    void execute(String message);
+}
