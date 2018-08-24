27b49d47ceeae4091c3f3264eccbb391999dfdb6
0217319 <xiaotantjl@163.com>
2018-08-24 18:42:40 +0800

    锁的学习

---
+++ xiangxue/src/main/java/lockDemo/LockDeom.java
@@ -0,0 +1,28 @@
+package lockDemo;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author 0217319
+ * @version V1.0
+ * @Description: 显示锁的demo
+ * @date
+ */
+public class LockDeom {
+    private Lock lock = new ReentrantLock();
+    int a = 0;
+    public void add(){
+        lock.lock();
+        try{
+            a++;
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+    public synchronized void add2(){
+            a++;
+    }
+}
