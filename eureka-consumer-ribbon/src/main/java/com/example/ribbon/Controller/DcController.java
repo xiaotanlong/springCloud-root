5e9ed797b574f82d74f48b79fa96062057b5dd1a
0217319 <xiaotantjl@163.com>
2018-08-27 19:45:51 +0800

    模块管理

--- eureka-consumer-ribbon/src/main/java/com/example/ribbon/Controller/DcController.java
+++ eureka-consumer-ribbon/src/main/java/com/example/ribbon/Controller/DcController.java
@@ -1,6 +1,7 @@
 package com.example.ribbon.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -22,8 +23,12 @@ public class DcController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;//设置随机访问策略
+
     @GetMapping("/consumer")
     public String dc() {
+        //this.loadBalancerClient.choose("service-B");//设置随机访问策略
         return restTemplate.getForObject("http://eureka-client/dc", String.class);
     }
 }
